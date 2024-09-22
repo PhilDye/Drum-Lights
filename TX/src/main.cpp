47fab0ce09a7e68200516ada47c39257f5f4d236
Phil Dye <phil@phildye.org>
2024-09-22 16:44:43 +0100

    fix: random auto initialiser

--- TX/src/main.cpp
+++ TX/src/main.cpp
@@ -47,7 +47,7 @@ const unsigned long AUTO_TIME = 30000; // in mS
 const int AUTO_MODE = -1;
 millisDelay autoDelay; // the delay object
 
-const int autoModes[21] = {
+const int autoModes[24] = {
       1,2,3,4,5,6,7,8,
       11,12,13,14,15,16,17,18,
       81,82,83,84,85,86,87,88
